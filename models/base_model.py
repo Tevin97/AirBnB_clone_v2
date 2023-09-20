17e863e6ea8959337763e826d513542857c32dd3
Abdulhafiz Gazali Ado <ahgazali108@gmail.com>
2023-09-20 17:34:21 +0000

    task3

--- models/base_model.py
+++ models/base_model.py
@@ -39,6 +39,4 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        if "_sa_instance_state" in dictionary:
-            dictionary.pop("_sa_instance_state")
         return dictionary
---
+++ setup_mysql_dev.sql
@@ -0,0 +1,8 @@
+--Prepares a MySql server
+--Creates a database, new user with password and priviledges
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'
+IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
