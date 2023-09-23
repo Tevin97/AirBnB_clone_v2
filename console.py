b3aa0fa678f89a25fa7880332262825d8d6cbd0a
Abdulhafiz Gazali Ado <ahgazali108@gmail.com>
2023-09-23 17:43:33 +0000

    Errors fixed

--- console.py
+++ console.py
@@ -144,7 +144,7 @@ class HBNBCommand(cmd.Cmd):
 
             parameter[key] = value
         new_instance = HBNBCommand.classes[class_name](**parameter)
-
+        storage.new(new_instance)
         storage.save()
         print(new_instance.id)
 
@@ -228,11 +228,11 @@ class HBNBCommand(cmd.Cmd):
             if args not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in storage.all(HBNBCommand.classes[args]).items():
                 if k.split('.')[0] == args:
                     print_list.append(str(v))
         else:
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in storage.all().items():
                 print_list.append(str(v))
 
         print(print_list)
