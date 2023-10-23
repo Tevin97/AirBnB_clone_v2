92a2fb679767ff4a516e776e59c1b2f47163dae7
Tevin <tevinmurimimwenda@gmail.com>
2023-10-22 18:19:28 -0700

    update

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,12 +6,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 # Defile the route for the root URL '/'
 @app.route('/', strict_slashes=False)
 def hello_world():
     """ Returns "Hello HBNB" """
     return 'Hello HBNB!'
 
+
 if __name__ == '__main__':
-    #Start the Flask server, listen on 0.0.0.0 (all networks) and port 5000
+    # Start the Flask server, listen on 0.0.0.0 (all networks) and port 5000
     app.run(host='0.0.0.0', port=5000)
