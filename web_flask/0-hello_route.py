4e1b37a785054f19ce456b295e51a539d76b125d
Tevin Murimi Mwenda <tevinmurimimwenda@gmail.com>
2023-11-19 02:13:33 +0000

    Update

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 """ starts a Flask web application  """
 
-from flask import Flask
+from flask import Flask, render_template
 
 
-app = Flask(__name__)
+app = Flask("__name__")
 
 
 # Defile the route for the root URL '/'
 @app.route('/', strict_slashes=False)
-def hello_world():
+def hello():
     """ Returns "Hello HBNB" """
-    return 'Hello HBNB!'
+    return render_template("10-hbnb_filters.html")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Start the Flask server, listen on 0.0.0.0 (all networks) and port 5000
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
